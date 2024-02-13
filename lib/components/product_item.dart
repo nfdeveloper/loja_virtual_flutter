5cfdac734acaa4930900f9086d02109f4f2df6b5
nfdeveloper <nfdeveloper04@gmail.com>
2024-02-13 12:07:50 -0300

    Criando componente Item de Produto

---
+++ lib/components/product_item.dart
@@ -0,0 +1,39 @@
+import 'package:flutter/material.dart';
+import 'package:lojavirtual/models/product.dart';
+
+class ProductItem extends StatelessWidget {
+  final Product product;
+
+  const ProductItem({
+    super.key,
+    required this.product,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return ClipRRect(
+      borderRadius: BorderRadius.circular(10),
+      child: GridTile(
+        child: Image.network(
+          product.imageUrl,
+          fit: BoxFit.cover,
+        ),
+        footer: GridTileBar(
+          backgroundColor: Colors.black54,
+          leading: IconButton(
+            onPressed: () {},
+            icon: const Icon(Icons.favorite),
+          ),
+          title: Text(
+            product.title,
+            textAlign: TextAlign.center,
+          ),
+          trailing: IconButton(
+            onPressed: () {},
+            icon: const Icon(Icons.shopping_cart),
+          ),
+        ),
+      ),
+    );
+  }
+}
