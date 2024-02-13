31194fb1dd971491df78fd40ff5b190626382127
nfdeveloper <nfdeveloper04@gmail.com>
2024-02-13 11:48:06 -0300

    Criando página de Listagem de Produtos

---
+++ lib/pages/products_overview_page.dart
@@ -0,0 +1,30 @@
+import 'package:flutter/material.dart';
+import 'package:lojavirtual/data/dummy_data.dart';
+import 'package:lojavirtual/models/product.dart';
+
+class ProductsOverviewPage extends StatelessWidget {
+  final List<Product> loadedProducts = dummyProducts;
+  ProductsOverviewPage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text("Minha Loja"),
+      ),
+      body: Padding(
+        padding: const EdgeInsets.all(10),
+        child: GridView.builder(
+          itemCount: loadedProducts.length,
+          itemBuilder: (ctx, i) => Text(loadedProducts[i].title),
+          gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
+            crossAxisCount: 2,
+            childAspectRatio: 3 / 2,
+            crossAxisSpacing: 10,
+            mainAxisSpacing: 10,
+          ),
+        ),
+      ),
+    );
+  }
+}
